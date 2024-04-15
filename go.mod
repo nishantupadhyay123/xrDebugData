360de431c5a9ce9fb07a8db0f6246ca7af9bfec1
niupadhy <niupadhy@cisco.com>
2024-04-15 11:08:23 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module XrBinary
+
+go 1.22.1
+
+require (
+	github.com/influxdata/telegraf v1.30.1
+	google.golang.org/protobuf v1.33.0
+)
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/klauspost/pgzip v1.2.6 // indirect
+)
---
+++ src/services/xrsnapshot.go
@@ -0,0 +1,144 @@
+/* ----------------------------------------------------------------------------
+* xr_debu_client - XR debug data TCP client
+* Author : Nishant Upadhyay
+* Arpil 2024,Copyright (c) 2016 by Cisco Systems, Inc.
+* ----------------------------------------------------------------------------
+ */
+package XrDebugData
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	_ "embed"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"text/scanner"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+type  XrDebugData struct {
+	// common configuration
+	Transport      string 
+	serviceAddress string `toml:"service_address"`
+	listner        net.Listener
+
+	// TCP related configuration
+	AllowedPendingMessages int `toml:"allowed_pending_messages"`
+	MaxTCPConnections      int `toml:"max_tcp_connections"`
+
+	// Internal state 
+	all_conn     []*net.Conn
+	acc   		   telegraf.Accumulator
+	cancel 		   context.CancelFunc
+	ctx 		   context.Context
+	wg    		   sync.WaitGroup			
+}
+
+// refrenced to https://pkg.go.dev/github.com/influxdata/telegraf#ServiceInput 
+
+func (x *XrDebugData) Start (acc telegraf.Accumulator) error {
+	var err error
+	x.acc = acc
+	x.ctx, x.cancel = context.WithCancel(context.Background())
+
+	// Currently support TCP only. In future Add 
+	if x.Transport != "TCP" {
+		return fmt.Errorf("E! Invalid  transport for XrDebugData: %s", x.Transport)
+	}
+	x.listner, err = net.Listen("tcp", x.serviceAddress)
+	if err != nil {
+		return err
+	}
+	x.wg.Add(1)
+	go x.acceptTCPClients() // Add the function for netaddress and multiple 
+	log.Printf(" Start the xrDebug Plugin at : %s", x.serviceAddress)
+	return nil
+}
+
+func (x *XrDebugData) acceptTCPClients() error {
+	// Add the code to accept tcp
+	// send the payload buffer to decode and store
+	var mutex sync.Mutex
+	clients := make(map[net.Conn]struct{})
+	for {
+		conn , err := x.listner.Accept()
+		if err != nil {
+			if x.ctx.Err() != nil {
+				break
+			}
+			x.acc.AddError(fmt.Errorf("E! Failed to accept TCP connection: %v", err))
+		}
+		mutex.Lock()
+		clients[conn] = struct{}{}
+		mutex.Unlock()
+		// Process 1 connection 
+		x.wg.Add(1)
+		go func (){
+			log.Printf("D! Accepted Cisco XrDebugData connection from %s", conn.RemoteAddr())
+			scanner := bufio.NewScanner(conn)
+			x.decodeMessage(payload.Bytes())
+
+		}()
+		log.Printf("D! Closed TCP dialout connection from %s", conn.RemoteAddr())
+		mutex.Lock()
+		delete(clients, conn)
+		mutex.Unlock()
+		conn.Close()
+		x.wg.Done()
+	}
+	return nil
+}
+
+
+// decode the bytes according to protobuf.
+func (x *XrDebugData) decodeMessage(data []byte) error {
+	return nil
+}
+
+// ##### telegraf input plugin settings
+
+const sampleConfig = `
+ ## transport type (oneof TCP and future gRPC)
+ Transport = "TCP"
+
+ ## Port on which XrDebugData will listen to
+ service_address : "5900" 
+
+ ## No. of tcp outstanding messages be processed.
+ allowed_pending_messages : 100000
+  
+ ## Max. no of TCP connections supported.Need to do a little research but we should tune this.
+ max_tcp_connections = 200
+`
+
+// SampleConfig of plugin
+func (x *XrDebugData) SampleConfig() string {
+	return sampleConfig
+}
+
+// Description of plugin
+func (x *XrDebugData) Description() string {
+	return "IOS-XR debug data plugin"
+}
+
+func ( x *XrDebugData) Gather (_ telegraf.Accumulator) error {
+	return nil
+}
+
+
+func init() {
+	inputs.Add("xrdebugdata", func() telegraf.Input {
+		return &XrDebugData{
+			Transport:      "TCP", // future could be gRPC.
+			serviceAddress: ":5900",
+			AllowedPendingMessages: 100000,
+			MaxTCPConnections: 200,
+		}
+	})
+}
+
