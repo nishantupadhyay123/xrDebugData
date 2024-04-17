edd3c2490b2fac07199562ed031b3e0d00ca4b5d
Nishant Upadhyay <niupadhy@cisco.com>
2024-04-16 19:45:15 -0700

    More changes for server side

--- generate.sh
+++ generate.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 protoc  -I src/ --go_out=src/ src/xrbinarypb/data.proto
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    src/xrbinarysrvpb/serv.proto
